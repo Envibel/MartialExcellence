50d00fc6c9454807aa638fc4f78cabde4a11b46f
Teraunce <teraunce@gmail.com>
2023-03-02 14:05:04 -0500

    Updated to work with 2.10b
    I have no life but to update mods it seems.

--- MartialExcellence/Races/Skinwalker/Heritages/SkinwalkerClassic.cs
+++ MartialExcellence/Races/Skinwalker/Heritages/SkinwalkerClassic.cs
@@ -4,6 +4,7 @@ using BlueprintCore.Blueprints.Configurators.Classes;
 using BlueprintCore.Blueprints.Configurators.Classes.Selection;
 using BlueprintCore.Blueprints.CustomConfigurators;
 using BlueprintCore.Blueprints.CustomConfigurators.Classes;
+using BlueprintCore.Blueprints.CustomConfigurators.Classes.Selection;
 using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Abilities;
 using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
 using BlueprintCore.Blueprints.References;
